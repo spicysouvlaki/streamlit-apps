9d42ceb8d78adf5420cec2a30ade7a81ff043478
William Schmitt <will@streamlit.io>
2021-03-15 17:37:06 -0700

    annotate errors

--- badreqs1/app.py
+++ badreqs1/app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.write("hello, world")
+st.write("""requirements.txt:
+streamlit==0.77.0
+streamlitX====0.0.1 <-- doesnt exist
+""")
--- badreqs2/app.py
+++ badreqs2/app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.write("hello, world")
+st.write("""this app has a bad requirements.txt
+streamlitX====0.0.1
+streamlit==0.77.0
+         """)
