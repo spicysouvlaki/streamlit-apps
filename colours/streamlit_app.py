92ee19a942767fd405e12d4fa475c815323bee8b
William Schmitt <will@streamlit.io>
2021-03-24 15:18:24 -0700

    add some flavour to logs

--- colours/streamlit_app.py
+++ colours/streamlit_app.py
@@ -8,9 +8,12 @@ st.title("this app prints in color! :rainbow:")
 
 st.write("stderr is a tty",sys.stderr.isatty() )
 st.write("stdout is a tty",sys.stdout.isatty() )
-print('\033[4;35m purple')
-logging.info('\033[4;35m purple')
+# print('\033[4;35m purple')
+# logging.info('\033[4;35m purple')
 
+logging.info("beep boop")
+logging.error("boop beep")
+logging.warning("ACK ACK!!")
 printInColor = st.checkbox("press to log colours")
 justPrint = st.checkbox("just print")
 
