eb714938dad7c5ffc7875d7f41d4c780b0bb9aac
William Schmitt <will@streamlit.io>
2021-03-24 11:54:09 -0700

    wip

--- colours/streamlit_app.py
+++ colours/streamlit_app.py
@@ -5,7 +5,8 @@ coloredlogs.install()
 
 st.title("this app prints in color! :rainbow:")
 
-print('\033[4;35m')
+print('\033[4;35m purple')
+logging.info('\033[4;35m purple')
 
 printInColor = st.checkbox("press to log colours")
 justPrint = st.checkbox("just print")
