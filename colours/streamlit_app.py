32806a7e63fb3752579e174dfb95a5f4b43296c2
William Schmitt <will@streamlit.io>
2021-04-05 11:24:37 -0700

    adds I love Streamlit

--- colours/streamlit_app.py
+++ colours/streamlit_app.py
@@ -8,12 +8,11 @@ st.title("this app prints in color! :rainbow:")
 
 st.write("stderr is a tty",sys.stderr.isatty() )
 st.write("stdout is a tty",sys.stdout.isatty() )
-# print('\033[4;35m purple')
-# logging.info('\033[4;35m purple')
 
 logging.info("beep boop")
 logging.error("boop beep")
 logging.warning("ACK ACK!!")
+logging.debug("I love Streamlit")
 printInColor = st.checkbox("press to log colours")
 justPrint = st.checkbox("just print")
 
